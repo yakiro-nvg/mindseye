0ef20f4dc24899f5e664a5ae6f9292f0c9bb38ca
Nguyen, Giang (G. Yakiro) <yakiro.nvg@gmail.com>
2019-09-07 02:52:06 -0400

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -4,6 +4,7 @@ wget https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
 unzip -j build-wrapper-linux-x86.zip
 
 export TRAVIS=true
+export PLATFORM=linux
 cd /root
 meson --cross-file=cross/aarch64.txt build
 cd build && /sonar/build-wrapper-linux-x86-64 --out-dir bw-output ninja
