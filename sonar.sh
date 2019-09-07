8ec2a1ff4d277073c02af0559c4e28bc6e4962b1
Nguyen, Giang (G. Yakiro) <yakiro.nvg@gmail.com>
2019-09-07 02:44:37 -0400

    update travis

---
+++ sonar.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+mkdir /sonar && cd /sonar
+wget https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+unzip -j build-wrapper-linux-x86.zip
+
+export TRAVIS=true
+cd /root
+meson --cross-file=cross/aarch64.txt build
+cd build && /sonar/build-wrapper-linux-x86-64 --out-dir bw-output ninja
