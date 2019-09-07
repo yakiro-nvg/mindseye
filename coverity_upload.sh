f26647da7115e5e8062b6227b349d87f4b4c8b51
Nguyen, Giang (G. Yakiro) <yakiro.nvg@gmail.com>
2019-09-07 15:08:52 -0400

    Update coverity_upload.sh

--- coverity_upload.sh
+++ coverity_upload.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+curl --form token=${COVERITY_SCAN_TOKEN} \
+  --form email=yakiro.nvg@gmail.com \
+  --form file=@mindseye.tgz \
+  https://scan.coverity.com/builds?project=yakiro-nvg%2Fmindseye
